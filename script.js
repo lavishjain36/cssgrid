695224e7ca4229d31684f655fcd69178e774b4e1
lavish jain <lavishjain36@gmail.com>
2021-09-06 12:08:48 +0530

    Added html forms

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-//use the set attributs navbar to topnavigation
+function updateText(val) {
+  document.getElementById("textinput").value = val;
+}
 
-var parent = document.getElementById("navBar");
-parent.setAttribute("id", "topnavigation");
-
-//2point
-var element = document.createElement("li");
-var textnode = document.createTextNode("Technology We use");
-//append
-
-element.appendChild(textnode);
-
-parent.firstElementChild.appendChild(element);
-
-var ul = document.getElementById("topnavigation").firstElementChild;
-
-//get the hold of first and last child under ul
-
-let first = ul.firstElementChild;
-let last = ul.lastElementChild;
-
-document.write(first.firstChild.firstChild.nodeValue + "<br>");
-document.write(last.firstChild.nodeValue + "<br>");
+//create function
